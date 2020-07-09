d38f24c8edce47fae167b0c93dbbe077d5e1a259
richard-mcghee <richardjmcghee@yahoo.com>
2020-07-09 01:11:41 -0400

    added some selectors, initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+const navLink = document.querySelectorAll('nav a');
+const logoHeading = document.querySelector('.logo-heading');
+const buttons = document.querySelectorAll('.btn');
+const mainBody = document.querySelector('body');
+const images = document.querySelectorAll('img');
+const input = document.querySelector('.form-submit');
+const parentContainer = document.querySelector('.container');
+const mainHome = document.querySelector('.home');
+const navBar = document.querySelector('nav');
